862c6e648b2d43d184c8af8963ce284921b0df20
Aexbrayat <exbrayat.antoni@gmail.com>
2019-10-11 10:10:43 +0200

    Update complete_taxo.py

--- snakevirome/scripts/complete_taxo.py
+++ snakevirome/scripts/complete_taxo.py
@@ -7,26 +7,32 @@ import subprocess
 
 Taxonomy_file=sys.argv[1]
 Taxonomy_full_file=sys.argv[2]
-Ref_taxonomy_file=sys.argv[3]
-ICTV_taxo_file=sys.argv[4]
-Host_taxo_file=sys.argv[5]
-output_stat= sys.argv[6]
-output_seq= sys.argv[7]
+Ref_taxonomy_file1=sys.argv[3]
+Ref_taxonomy_file=sys.argv[4]
+ICTV_taxo_file=sys.argv[5]
+Host_taxo_file=sys.argv[6]
+output_stat= sys.argv[7]
+output_seq= sys.argv[8]
 
 
 ref_lineage=[]
+ref_lineage1=[]
 ICTV_lineage=[]
 host_lineage=[]
 host_dict={}
 Taxonomy=open(Taxonomy_file, 'r')
 Taxonomy_full=open(Taxonomy_full_file, 'r')
 Ref_taxonomy=open(Ref_taxonomy_file, 'r')
+Ref_taxonomy1=open(Ref_taxonomy_file1, 'r')
 ICTV_taxo=open(ICTV_taxo_file, 'r')
 host_taxo=open(Host_taxo_file, 'r')
 
 for lineage in Ref_taxonomy:
 	ref_lineage.append(lineage.replace('\n','').split(','))
-#
+
+for lineage in Ref_taxonomy1:
+	ref_lineage1.append(lineage.replace('\n','').split(','))
+
 for lineage in host_taxo:
 	host_lineage=(lineage.replace('\n','').split(','))
 	if host_lineage[0] != "virus_tax_id":
@@ -42,43 +48,64 @@ seqid=[]
 for lineage in Taxonomy_full:
 	rank=lineage.replace('\n','').replace('\r','').split("	")
 	if rank[0]=="qseqid":
-		ranks_full=rank[0:16]+["clade"]+rank[16:21]+["taxon"]+["Nucleic_acid"]+rank[21:22]+["hosts_gb"]+["hosts_tax_id"]+["hosts_superkingdom"]+["hosts_class"]+["hosts_order"]+["hosts_family"]+["hosts_subfamily"]+["hosts_genus"]+["hosts_species"]+rank[22:]
+		ranks_full=rank[0:16]+["clade"]+rank[16:21]+["cluster"]+["Nucleic_acid"]+rank[21:22]+["hosts_gb"]+["hosts_tax_id"]+["hosts_superkingdom"]+["hosts_class"]+["hosts_order"]+["hosts_family"]+["hosts_subfamily"]+["hosts_genus"]+["hosts_species"]+rank[22:]
 	elif rank[14]=="Viruses":
-		ranks_full=rank[0:16]+["<not_present>"]+rank[16:21]+["<not_present>"]+["<not_present>"]+rank[21:22]+["<not_present>"]+["<not_present>"]+["<not_present>"]+["<not_present>"]+["<not_present>"]+["<not_present>"]+["<not_present>"]+["<not_present>"]+["<not_present>"]+rank[22:]
+		ranks_full=rank[0:16]+["NA"]+rank[16:21]+["NA"]+["NA"]+rank[21:22]+["NA"]+["NA"]+["NA"]+["NA"]+["NA"]+["NA"]+["NA"]+["NA"]+["NA"]+rank[22:]
 	else:
 		continue
 	liste_full.append(ranks_full)
+
 host_all=[]
 for ranks_full in liste_full:
 	index=liste_full.index(ranks_full)
-	if ranks_full[14]=="Viruses":
-		if ranks_full[20]!="<not_present>" and ranks_full[19]=="<not_present>":
-			for ref_rank in ref_lineage:
-				if ranks_full[20]==ref_rank[3]:
+	if liste_full[index][14]=="Viruses":
+		for ictv_rank in ICTV_lineage:
+			if liste_full[index][21]==ictv_rank[3] and  liste_full[index][21]!="NA" :
+				if liste_full[index][20]=="NA":
+					liste_full[index][20]=ictv_rank[2]
+				if liste_full[index][18]=="NA":
+					liste_full[index][18]=ictv_rank[1]
+				if liste_full[index][17]=="NA":
+					liste_full[index][17]=ictv_rank[0]
+				if liste_full[index][23]=="NA":
+					liste_full[index][23]=ictv_rank[4]
+
+		for ref_rank in ref_lineage1:
+			if liste_full[index][20]==ref_rank[3] and  liste_full[index][20]!="NA" :
+				if liste_full[index][18]=="NA":
 					liste_full[index][18]=ref_rank[2]
+				if liste_full[index][17]=="NA":
 					liste_full[index][17]=ref_rank[1]
-					liste_full[index][16]=ref_rank[0]
-					liste_full[index][22]=ref_rank[4]
+				if liste_full[index][23]=="NA":
 					liste_full[index][23]=ref_rank[5]
-		if ranks_full[18]!="<not_present>" and ranks_full[17]=="<not_present>":
-			for ref_rank in ref_lineage:
-				if ranks_full[18]==ref_rank[2]:
-					liste_full[index][17]=ref_rank[1]
-					liste_full[index][16]=ref_rank[0]
+				if liste_full[index][22]=="NA":
 					liste_full[index][22]=ref_rank[4]
-					liste_full[index][23]=ref_rank[5]
-		if ranks_full[18]!="<not_present>" and liste_full[index][22]=="<not_present>":
-			liste_full[index][22]=ranks_full[18]
-		if liste_full[index][22]=="<not_present>" and ranks_full[20]!="<not_present>" :
-			liste_full[index][22]=ranks_full[20]
-		if liste_full[index][22]=="<not_present>" and ranks_full[17]!="<not_present>" :
-			liste_full[index][22]=rank[17]
-		if liste_full[index][23]=="<not_present>":
-			if ranks_full[20]!="<not_present>":
-				for ictv_rank in ICTV_lineage:
-					if ranks_full[20]==ictv_rank[2]:
-						liste_full[index][23]=ictv_rank[4]
-		if liste_full[index][23]=="<not_present>":
+				if liste_full[index][16]=="NA":
+					liste_full[index][16]=ref_rank[0]
+
+		for ref_rank1 in ref_lineage:
+			if liste_full[index][21]==ref_rank1[6] and  liste_full[index][21]!="NA" :
+				if liste_full[index][20]=="NA":
+					liste_full[index][20]=ref_rank1[3]
+				if liste_full[index][18]=="NA":
+					liste_full[index][18]=ref_rank1[2]
+				if liste_full[index][17]=="NA":
+					liste_full[index][17]=ref_rank1[1]
+				if liste_full[index][23]=="NA":
+					liste_full[index][23]=ref_rank1[5]
+				if liste_full[index][22]=="NA":
+					liste_full[index][22]=ref_rank1[4]
+				if liste_full[index][16]=="NA":
+					liste_full[index][16]=ref_rank1[0]
+
+		if liste_full[index][18]!="NA" and liste_full[index][22]=="NA":
+			liste_full[index][22]=liste_full[index][18]
+		if liste_full[index][7]!="NA" and liste_full[index][22]=="NA":
+			liste_full[index][22]=liste_full[index][7]
+		if liste_full[index][17]!="NA" and liste_full[index][22]=="NA":
+			liste_full[index][22]=liste_full[index][17]
+
+		if liste_full[index][23]=="NA":
 			if "ssRNA negative-strand viruses" in liste_full[index][24]:
 				liste_full[index][23]="ssRNA(-)"
 			if "ssRNA positive-strand viruses" in liste_full[index][24]:
@@ -94,26 +121,69 @@ for ranks_full in liste_full:
 			if "unclassified ssRNA viruses" in liste_full[index][24]:
 				liste_full[index][23]="ssRNA"
 			if "unclassified RNA viruses" in liste_full[index][24]:
-				liste_full[index][23]="RNA"
-		if liste_full[index][1] not in seqid :
+				 liste_full[index][23]="RNA"
+		if  liste_full[index][1] not in seqid :
 			seqid.append(liste_full[index][1])
+
+		if ranks_full[13] in host_dict:
+			host_id=""
+			superkingdom=[]
+			clas=[]
+			order=[]
+			family=[]
+			subfamily=[]
+			genus=[]
+			species=[]
+			for hosts in host_dict[ranks_full[13] ]:
+				if host_id=="" and hosts[0]!='':
+					host_id+=str(hosts[0]).replace("[","").replace("]","")
+				else:
+					host_id+="|"+str(hosts[0])
+				if hosts[1] not in superkingdom:
+					superkingdom.append(hosts[1])
+				if hosts[2] not in clas:
+					clas.append(hosts[2])
+				if hosts[3] not in order:
+					order.append(hosts[3])
+				if hosts[4] not in family:
+					family.append(hosts[4])
+				if hosts[5] not in subfamily:
+					subfamily.append(hosts[5])
+				if hosts[6] not in genus:
+					genus.append(hosts[6])
+				if hosts[7] not in species:
+					species.append(hosts[7])
+			liste_full[index][26]=(str(host_id).replace("']","").replace("['",""))
+			liste_full[index][27]=(str(superkingdom).replace("]","").replace("[","").replace("'","").replace(", ","|"))
+			liste_full[index][28]=(str(clas).replace("]","").replace("[","").replace("'","").replace(", ","|"))
+			liste_full[index][29]=(str(order).replace("]","").replace("[","").replace("'","").replace(", ","|"))
+			liste_full[index][30]=(str(family).replace("]","").replace("[","").replace("'","").replace(", ","|"))
+			liste_full[index][31]=(str(subfamily).replace("]","").replace("[","").replace("'","").replace(", ","|"))
+			liste_full[index][32]=(str(genus).replace("]","").replace("[","").replace("'","").replace(", ","|"))
+			liste_full[index][33]=(str(species).replace("]","").replace("[","").replace("'","").replace(", ","|"))
+
+
 ncbi = NCBITaxa()
 liste_host=[]
 stat_by_seq=[]
 n=0
+
+print("ok2")
 desired_ranks = ['superkingdom', 'class', 'order', 'family', 'subfamily','genus','species']
 for ids in seqid:
+	print(n,"/",len(seqid))
 	n+=1
 	ranks2lineage={}
 	liste=[]
 	fieldnames=[]
 	fieldnames1=[]
 	host_list=[]
-	host_id="<not_present>"
+	host_id="NA"
 	command = 'esearch -db protein -query '+ str(ids) +'| efetch -format gb | grep -oP "/host=\K.*"'
 	proc = subprocess.Popen(command,stdout=subprocess.PIPE,shell=True)
 	(out, err) = proc.communicate()
 	host_gb=out.decode("utf-8").rstrip().replace('"','').replace("[u'","")
+	host_gb=""
 	cleaned_host_gb=host_gb.split(' (', 1)[0]
 	if cleaned_host_gb=="mosquito" or cleaned_host_gb=="mosquitoes":
 		cleaned_host_gb="Culicoidea"
@@ -144,9 +214,12 @@ for ids in seqid:
 			ranks2names[rank]=ranks2names[rank][:-1]
 		ranks2names['host_id']=host_id
 		for rank in desired_ranks:
-			fieldnames1.append(ranks2names.get(rank, '<not_present>'))
+			fieldnames1.append(ranks2names.get(rank, 'NA'))
 	host_list=[ids]+fieldnames+fieldnames1
 	host_all.append(host_list)
+
+
+print("ok3")
 dict_host={}
 for seq in liste_full:
 	if seq[0] == "qseqid":
@@ -171,52 +244,81 @@ for seq in liste_full:
 							dict_host[seq[13]]={idx_host:[val_host]}
 						elif idx_host not in dict_host[seq[13]]:
 							dict_host[seq[13]].update({idx_host:[val_host]})
-						elif  val_host!='<not_present>' and val_host not in dict_host[seq[13]][idx_host]:
+						elif  val_host!='NA' and val_host not in dict_host[seq[13]][idx_host]:
 							dict_host[seq[13]][idx_host].append(val_host)
-# dict_host={}
-# for seq in stat_by_seq:
-# 	if seq[13] not in dict_host
+
+print(dict_host)
+for ranks_full in liste_full:
+	index=liste_full.index(ranks_full)
+	if str(liste_full[index][13]) in dict_host and len(dict_host[str(liste_full[index][13])])>2:
+		liste_full[index][25]=str(dict_host[str(liste_full[index][13])][2]).replace("]","").replace("[","").replace("'","").replace(", ","|")
+		liste_full[index][26]=str(dict_host[str(liste_full[index][13])][3]).replace("]","").replace("[","").replace("'","").replace(", ","|")
+		liste_full[index][27]=str(dict_host[str(liste_full[index][13])][4]).replace("]","").replace("[","").replace("'","").replace(", ","|")
+		liste_full[index][28]=str(dict_host[str(liste_full[index][13])][5]).replace("]","").replace("[","").replace("'","").replace(", ","|")
+		liste_full[index][29]=str(dict_host[str(liste_full[index][13])][6]).replace("]","").replace("[","").replace("'","").replace(", ","|")
+		liste_full[index][30]=str(dict_host[str(liste_full[index][13])][7]).replace("]","").replace("[","").replace("'","").replace(", ","|")
+		liste_full[index][31]=str(dict_host[str(liste_full[index][13])][8]).replace("]","").replace("[","").replace("'","").replace(", ","|")
+		liste_full[index][32]=str(dict_host[str(liste_full[index][13])][9]).replace("]","").replace("[","").replace("'","").replace(", ","|")
+
 liste_species=[]
 for lineage in Taxonomy:
+
 	rank=lineage.replace('\n','').replace('\r','').split("	")
 	if rank[0]=="tax_id":
-		ranks=rank[0:3]+["clade"]+rank[3:8]+["taxon"]+["Nucleic_acid"]+["hosts_gb"]+["hosts_tax_id"]+["hosts_superkingdom"]+["hosts_class"]+["hosts_order"]+["hosts_family"]+["hosts_subfamily"]+["hosts_genus"]+["hosts_species"]+rank[9:]
+		ranks=rank[0:3]+["clade"]+rank[3:8]+["cluster"]+["Nucleic_acid"]+["hosts_gb"]+["hosts_tax_id"]+["hosts_superkingdom"]+["hosts_class"]+["hosts_order"]+["hosts_family"]+["hosts_subfamily"]+["hosts_genus"]+["hosts_species"]+rank[9:]
 	elif rank[1]=="Viruses":
-		ranks=rank[0:3]+["<not_present>"]+rank[3:8]+["<not_present>"]+["<not_present>"]+["<not_present>"]+["<not_present>"]+["<not_present>"]+["<not_present>"]+["<not_present>"]+["<not_present>"]+["<not_present>"]+["<not_present>"]+["<not_present>"]+rank[9:]
+		ranks=rank[0:3]+["NA"]+rank[3:8]+["NA"]+["NA"]+["NA"]+["NA"]+["NA"]+["NA"]+["NA"]+["NA"]+["NA"]+["NA"]+["NA"]+rank[9:]
 	else:
 		continue
 	liste_species.append(ranks)
 
+
 for ranks in liste_species:
 	index=liste_species.index(ranks)
-	if ranks[1]=="Viruses":
-		if ranks[7]!="<not_present>" and ranks[6]=="<not_present>":
-			for ref_rank in ref_lineage:
-				if ranks[7]==ref_rank[3]:
+	if liste_species[index][1]=="Viruses":
+		for ictv_rank in ICTV_lineage:
+			if liste_species[index][8]==ictv_rank[3] and liste_species[index][8]!="NA":
+				if liste_species[index][7]=="NA":
+					liste_species[index][7]=ictv_rank[2]
+				if liste_species[index][5]=="NA":
+					liste_species[index][5]=ictv_rank[1]
+				if liste_species[index][4]=="NA":
+					liste_species[index][4]=ictv_rank[0]
+				if liste_species[index][10]=="NA":
+					liste_species[index][10]=ictv_rank[4]
+
+		for ref_rank in ref_lineage1:
+			if liste_species[index][7]==ref_rank[3] and liste_species[index][7]!="NA":
+				if liste_species[index][5]=="NA":
 					liste_species[index][5]=ref_rank[2]
+				if liste_species[index][4]=="NA":
 					liste_species[index][4]=ref_rank[1]
-					liste_species[index][3]=ref_rank[0]
-					liste_species[index][9]=ref_rank[4]
+				if liste_species[index][10]=="NA":
 					liste_species[index][10]=ref_rank[5]
-		if ranks[5]!="<not_present>" and ranks[4]=="<not_present>":
-			for ref_rank in ref_lineage:
-				if ranks[5]==ref_rank[2]:
-					liste_species[index][4]=ref_rank[1]
-					liste_species[index][3]=ref_rank[0]
+				if liste_species[index][9]=="NA":
 					liste_species[index][9]=ref_rank[4]
-					liste_species[index][10]=ref_rank[5]
-		if ranks[5]!="<not_present>" and liste_species[index][9]=="<not_present>":
-			liste_species[index][9]=ranks[5]
-		if liste_species[index][9]=="<not_present>" and ranks[7]!="<not_present>" :
-			liste_species[index][9]=ranks[7]
-		if liste_species[index][9]=="<not_present>" and ranks[4]!="<not_present>" :
-			liste_species[index][9]=rank[4]
-		if liste_species[index][10]=="<not_present>":
-			if ranks[7]!="<not_present>":
-				for ictv_rank in ICTV_lineage:
-					if ranks[7]==ictv_rank[2]:
-						liste_species[index][10]=ictv_rank[4]
-		if liste_species[index][10]=="<not_present>":
+
+		for ref_rank1 in ref_lineage:
+			if liste_species[index][8]==ref_rank1[6] and liste_species[index][8]!="NA" :
+				if liste_species[index][7]=="NA":
+					liste_species[index][7]=ref_rank1[3]
+				if liste_species[index][5]=="NA":
+					liste_species[index][5]=ref_rank1[2]
+				if liste_species[index][4]=="NA":
+					liste_species[index][4]=ref_rank1[1]
+				if liste_species[index][10]=="NA":
+					liste_species[index][10]=ref_rank1[5]
+				if liste_species[index][9]=="NA":
+					liste_species[index][9]=ref_rank1[4]
+
+		if liste_species[index][5]!="NA" and liste_species[index][9]=="NA":
+			liste_species[index][9]=liste_species[index][5]
+		if liste_species[index][7]!="NA" and liste_species[index][9]=="NA":
+			liste_species[index][9]=liste_species[index][7]
+		if liste_species[index][4]!="NA" and liste_species[index][9]=="NA":
+			liste_species[index][9]=liste_species[index][4]
+
+		if liste_species[index][10]=="NA":
 			if "ssRNA negative-strand viruses" in liste_species[index][11]:
 				liste_species[index][10]="ssRNA(-)"
 			if "ssRNA positive-strand viruses" in liste_species[index][11]:
@@ -293,7 +395,7 @@ for listes in liste_species:
 	if listes[0]=="tax_id":
 		to_write.append(listes[0:11]+["host_taxon"]+listes[11:])
 	else:
-		to_write.append(listes[0:11]+["<not_present>"]+listes[11:])
+		to_write.append(listes[0:11]+["NA"]+listes[11:])
 	if "|" in listes[12] :
 		dic_host_ids[listes[0]]=(listes[12].split('|'))
 	else :
@@ -305,7 +407,7 @@ dic={}
 test=[]
 for key in dic_host_ids:
 	for ids in dic_host_ids[key]:
-		if ids != "<not_present>" and ids.isdigit():
+		if ids != "NA" and ids.isdigit():
 			lineage=ncbi.get_lineage(ids)
 			lineage2name = ncbi.translate_to_names(lineage)
 			lineage2ranks=ncbi.get_rank(lineage)
@@ -320,7 +422,7 @@ for key in dic_host_ids:
 						dic[key][rank].append(Dic_lineage2name[taxid])
 
 	if key in dic:
-		if 'kingdom' in dic[key] and len(dic[key]['kingdom'])==1 and dic[key]['kingdom'][0]!='<not_present>' and dic[key]['kingdom'][0]!='Metazoa':
+		if 'kingdom' in dic[key] and len(dic[key]['kingdom'])==1 and dic[key]['kingdom'][0]!='NA' and dic[key]['kingdom'][0]!='Metazoa':
 			for line in to_write :
 				if key in line:
 					line[11]=str(dic[key]['kingdom'][0])
@@ -340,7 +442,7 @@ for key in dic_host_ids:
 						if key in line:
 							line[11]=str(dic[key]['order'][0])
 		for line in to_write:
-			if key in line and  line[11]=='<not_present>' and 'class' in dic[key] and len(dic[key]['class'])==1:
+			if key in line and  line[11]=='NA' and 'class' in dic[key] and len(dic[key]['class'])==1:
 				if dic[key]['class'][0]=='Insecta':
 							line[11]=str(dic[key]['phylum'][0])
 				elif dic[key]['class'][0]!='Mammalia':
@@ -348,11 +450,12 @@ for key in dic_host_ids:
 								line[11]='Vertebrata'
 				elif dic[key]['class'][0]=='Mammalia':
 							line[11]=str(dic[key]['class'][0])
-			if key in line and  'phylum' in dic[key] and  line[11]=='<not_present>' and dic[key]['phylum'][0]=='Arthropoda':
+			if key in line and  'phylum' in dic[key] and  line[11]=='NA' and dic[key]['phylum'][0]=='Arthropoda':
 						line[11]='Arthropoda'
-			if key in line and  line[11]=='<not_present>':
+			if key in line and  line[11]=='NA':
 				if len(line[13].split('|'))==1:
 					line[11]='Other'
+
 with open(output_stat, "w") as f:
     writer = csv.writer(f)
     writer.writerows(to_write)
